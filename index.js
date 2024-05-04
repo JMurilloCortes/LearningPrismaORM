8f39f027a20351193c68e9e5c20ddf02f6232013
JMurilloCortes <jealmuco@gmail.com>
2024-05-04 17:04:22 -0500

    Obtener datos un solo registro

--- index.js
+++ index.js
@@ -13,12 +13,34 @@ async function main() {
   // });
   // console.log(newUser)
   
-  const allUsers = await prisma.user.findMany();
+  // const allUsers = await prisma.user.findMany();
   // console.log(allUsers)
 
-  allUsers.forEach((user) => {
-    console.log(`${user.id} - ${user.name}`)
+  // allUsers.forEach((user) => {
+  //   console.log(`${user.id} - ${user.name}`)
+  // })
+
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     id: "clvsmm4wd00001ll1gme2s77i",
+  //     email: "fenagles19@gmail.com"
+  //   }
+  // })
+  // console.log(user)
+
+  const user = await prisma.user.findFirst({
+    where: {
+      AND: [
+        { email: "fenagles19@gmail.com" },
+        { name: "Fe" }
+  ]
+}
   })
+  console.log(user)
+  
+
+
+
 }
 
 main()
