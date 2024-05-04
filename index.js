4ba4f9756587b90870be557a67632108076cfe19
JMurilloCortes <jealmuco@gmail.com>
2024-05-04 16:47:18 -0500

    Consultando datos

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const newUser = await prisma.user.create({
-    data: {
-      email: "fenagles19@gmail.com",
-      name: "Fe",
-      lastName: "Nagles",
-      password: "N1234",
-  }
-  });
-  console.log(newUser)
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     email: "fenagles19@gmail.com",
+  //     name: "Fe",
+  //     lastName: "Nagles",
+  //     password: "N1234",
+  // }
+  // });
+  // console.log(newUser)
+  
+  const allUsers = await prisma.user.findMany();
+  // console.log(allUsers)
+
+  allUsers.forEach((user) => {
+    console.log(`${user.id} - ${user.name}`)
+  })
 }
 
 main()
